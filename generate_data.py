00475c81f9761aecfafc6d71752432ca13b66587
Gabriel Paredes <gparedes@duodata.io>
2025-02-18 00:52:21 +0800

    [ADD] Calculate the total sales for each day of the week.

--- generate_data.py
+++ generate_data.py
@@ -1,29 +1,73 @@
-# TODO: add ratings
-# TODO: add number of items sales per week,
-# TODO: add customer ratings for each item,
+# DONE: add ratings
+# DONE: add number of items sales per week
+# DONE: add customer ratings for each item
+# DONE: add date
 
 # Import the required module
 import csv
+from faker import Faker
+
+
+def fake_food():
+    """
+    Returns a random food option from the menu with its price and category.
+
+    Returns:
+        list: A list containing the name of the food, its price and category.
+    """
+    foods = [
+        ['Chicken Parmigiana', 19.99, 'Main'],
+        ['Fish and Chips', 18.99, 'Main'],
+        ['Margherita Pizza', 15.99, 'Main'],
+        ['Caesar Salad', 9.99, 'Starter'],
+        ['Garlic Bread', 6.99, 'Starter'],
+        ['Tiramisu', 7.99, 'Dessert'],
+        ['Cheesecake', 6.99, 'Dessert']]
+    index = fake.random_int(min=0, max=len(foods)-1)
+    return foods[index]
+
+
+def fake_customer_rating(max):
+    """
+    Generate a list of customer ratings with names and ratings.
+
+    Args:
+        max (int): The maximum number of customer ratings to generate.
+
+    Returns:
+        list: A list of customer ratings. Each customer rating is a dictionary 
+              with keys "name" and "rating".
+    """
+    customers_rating = []
+    for i in range(max):
+        customers_rating.append(
+            {"name": fake.name(), "rating": fake.random_int(min=1, max=5)})
+    return customers_rating
+
+
+fake = Faker()
 # Define the field names
-fields = ['item', 'price', 'category',
-          'rating', 'sales_week', 'customer_rating']
+fields = ['item',
+          'price',
+          'category',
+          'rating',
+          'sales_week',
+          'customer_rating',
+          'date'
+          ]
+
 # Define the data rows
-rows = [
-    ['Chicken Parmigiana', 19.99, 'Main', '5',
-        '20', [{"name": "example", "rating": "5"}]],
-    ['Fish and Chips', 18.99, 'Main', '5', '20',
-        [{"name": "example", "rating": "5"}]],
-    ['Margherita Pizza', 15.99, 'Main', '5', '20',
-        [{"name": "example", "rating": "5"}]],
-    ['Caesar Salad', 9.99, 'Starter', '5', '20',
-        [{"name": "example", "rating": "5"}]],
-    ['Garlic Bread', 6.99, 'Starter', '5', '20',
-        [{"name": "example", "rating": "5"}]],
-    ['Tiramisu', 7.99, 'Dessert', '5', '20', [
-        {"name": "example", "rating": "5"}]],
-    ['Cheesecake', 6.99, 'Dessert', '5', '20',
-        [{"name": "example", "rating": "5"}]],
-]
+rows = []
+for i in range(25):
+    food = fake_food()
+    rows.append([food[0],
+                 food[1],
+                 food[2],
+                 0,
+                 fake.random_int(min=0, max=100),
+                 fake_customer_rating(fake.random_int(min=2, max=10)),
+                 fake.date()
+                 ])
 # Create a CSV file and write the data rows
 with open('menu.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
--- main.py
+++ main.py
@@ -1,4 +1,96 @@
-# Display the data in a table(or table-like)format.
-# Calculate the average rating for each menu item.
-# Calculate the total sales for each day of the week.
-# Determine the most popular menu item based on customer ratings
+# DONE: Display the data in a table(or table-like)format.
+# DONE: Calculate the average rating for each menu item.
+# DONE: Calculate the total sales for each day of the week.
+# TODO: Determine the most popular menu item based on customer ratings
+# TODO: Show details of customers_rating
+
+import csv
+from tabulate import tabulate
+import itertools
+
+
+def get_data():
+    """
+    Reads the menu.csv file and returns its content as a list of lists.
+
+    Returns:
+        list: A list of lists, where each sublist represents a row in the CSV file.
+    """
+    with open('menu.csv', 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+        return data
+
+
+def show_data(data):
+    """
+    Prints the menu data in a table format, with the following calculations:
+        - row numbers
+        - average customer rating for each menu item
+        - total of customer ratings for each menu item
+
+    Args:
+        data (list): A list of lists, where each sublist represents a row in the CSV file.
+    """
+    headers = data[0]
+    # Insert row numbers
+    rows = data[1:]
+    for i, row in enumerate(rows, start=1):
+        row.insert(0, i)
+    # Calculate how many customers rated each menu item
+
+    for row in data[1:]:
+
+        customer_rating = row[6]
+        rating_length = len(eval(customer_rating))
+
+        # Calculate average rating
+        avg_rating = 0
+        for row_rating in eval(customer_rating):
+            avg_rating += row_rating["rating"]
+        if rating_length > 0:
+            avg_rating = avg_rating / len(eval(customer_rating))
+        else:
+            avg_rating = 0
+
+        row[4] = avg_rating
+        # Calculate total of customer ratings
+
+        row[6] = rating_length
+    data = [headers] + rows
+    print(tabulate(data, headers='firstrow', tablefmt='grid'))
+
+
+def cal_sales_day(data):
+
+    # Sort data by date
+    data.sort(key=lambda x: x[6])
+
+    # Group data by date
+    grouped_data = itertools.groupby(data, key=lambda x: x[6])
+
+    # Print grouped data
+    for date, group in grouped_data:
+        print(f"Date: {date}") if date != "date" else None
+        sales_per_day = 0
+        for item in group:
+
+            try:
+                sales_per_day = sales_per_day + float(item[1])
+            except ValueError:
+                pass
+        print(f"{sales_per_day} AUD") if sales_per_day != 0 else None
+
+
+user_input = input("""
+                   Please enter something: \n
+                   1) Display data
+                   2) Calculate the total sales for each day of the week
+                   """)
+data = get_data()
+if user_input == "1":
+    show_data(data)
+elif user_input == "2":
+    cal_sales_day(data)
+else:
+    print()
